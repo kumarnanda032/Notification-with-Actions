33be2e40ec9146f5438f00a54f652f00dee87bd6
Nanda Kumar <kumar.nanda032@gmail.com>
2023-11-11 15:07:04 +0530

    final commit

--- app/src/main/java/com/example/notificationwithactions/MainActivity.java
+++ app/src/main/java/com/example/notificationwithactions/MainActivity.java
@@ -10,11 +10,11 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
-import android.os.Bundle;
-import android.view.View;
+
+
 import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
+
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
@@ -35,44 +35,45 @@ public class MainActivity extends AppCompatActivity {
 
         Button button = findViewById(R.id.button);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        button.setOnClickListener(v -> {
 
-                createNotificationChannel();
+            createNotificationChannel();
 
-                Intent homeIntent = new Intent(getApplicationContext(), MainActivity.class);
+            Intent homeIntent = new Intent(getApplicationContext(), MainActivity.class);
 
-                Intent webIntent = new Intent(Intent.ACTION_VIEW);
-                webIntent.setData(Uri.parse("http://www.google.com"));
+            Intent webIntent = new Intent(Intent.ACTION_VIEW);
+            webIntent.setData(Uri.parse("http://www.google.com"));
 
-                PendingIntent pendingIntentOne = PendingIntent.getActivity(getApplicationContext(), 1, homeIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-                PendingIntent pendingIntentTwo = PendingIntent.getActivity(getApplicationContext(), 1, webIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            PendingIntent pendingIntentOne = PendingIntent.getActivity(getApplicationContext(), 1, homeIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            PendingIntent pendingIntentTwo = PendingIntent.getActivity(getApplicationContext(), 1, webIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
 
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
-                        .setSmallIcon(R.drawable.notification)
-                        .setContentTitle("Notification With Action")
-                        .setShowWhen(true)
-                        .setAutoCancel(true)
-                        .setContentText("Android Studio Tutorials")
-                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                        .addAction(R.drawable.ic_baseline_home_24, "HOME", pendingIntentOne)
-                        .addAction(R.drawable.ic_baseline_web_24, "GOOGLE", pendingIntentTwo);
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
+                    .setSmallIcon(R.drawable.notification)
+                    .setContentTitle("Notification With Action")
+                    .setShowWhen(true)
+                    .setAutoCancel(true)
+                    .setContentText("Android Studio Tutorials")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .addAction(R.drawable.ic_baseline_home_24, "HOME", pendingIntentOne)
+                    .addAction(R.drawable.ic_baseline_web_24, "GOOGLE", pendingIntentTwo);
 
 
-                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
 
-                //notificationId is a unique int for each notification that you must define
-                if (ActivityCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-
-                    return;
-                }
-                notificationManager.notify(NOTIFICATION_ID, builder.build());
-
+            //notificationId is a unique int for each notification that you must define
+            if (ActivityCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+              
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
             }
+            notificationManager.notify(NOTIFICATION_ID, builder.build());
+
         });
 
 
