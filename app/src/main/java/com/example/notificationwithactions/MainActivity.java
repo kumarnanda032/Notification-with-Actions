9bdc0863ffb9eeaf7174c6234d8377262a09315d
Nanda Kumar <kumar.nanda032@gmail.com>
2023-11-11 15:04:31 +0530

    final commit

--- app/src/main/java/com/example/notificationwithactions/MainActivity.java
+++ app/src/main/java/com/example/notificationwithactions/MainActivity.java
@@ -67,11 +67,7 @@ public class MainActivity extends AppCompatActivity {
                 if (ActivityCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
                     // TODO: Consider calling
                     //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
+
                     return;
                 }
                 notificationManager.notify(NOTIFICATION_ID, builder.build());
